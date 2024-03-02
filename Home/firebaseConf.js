32603e9311420b8c628f30d3bd524b2aa372fd42
samade747 <samad.e747@gmail.com>
2024-03-02 17:52:48 +0500

    552

--- Home/firebaseConf.js
+++ Home/firebaseConf.js
@@ -3,13 +3,7 @@
 // import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyDnjNRjq583w2mqTLEPEgItZkUrHVN7SBA",
-//   authDomain: "first-frontend-project-da025.firebaseapp.com",
-//   projectId: "first-frontend-project-da025",
-//   storageBucket: "first-frontend-project-da025.appspot.com",
-//   messagingSenderId: "796702314001",
-//   appId: "1:796702314001:web:d4a8041c80b87c69b36bb0",
-//   measurementId: "G-JJGJY0FBXM"
+//   
 // };
 //   const app = initializeApp(firebaseConfig);
 // const db = getFirestore(app);
